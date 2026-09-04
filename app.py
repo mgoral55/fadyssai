350147fe358890f9a1e4cac047b78ad11d334e38
Magda <m.goral55@gmail.com>
2026-09-04 22:43:24 +0200

    Model 3.1-lite

--- app.py
+++ app.py
@@ -1203,7 +1203,7 @@ with st.sidebar:
         "Model Gemini", 
         options=[
             "gemini-3.5-flash",
-            "gemini-3.1-pro",
+            "gemini-3.1-flash-lite",
             "gemini-3.5-flash-lite",
             "gemini-3.6-flash"
         ], 
